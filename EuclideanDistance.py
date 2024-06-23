fb57d6177d0441eb9281f12e61c6dfad59bf487e
Elif Yıldız <112424832+efyldz@users.noreply.github.com>
2024-06-23 22:58:01 +0300

    Update EuclideanDistance.py

--- EuclideanDistance.py
+++ EuclideanDistance.py
@@ -6,7 +6,7 @@ x_2= int(input("Enter second x value:  "))
 y_2= int(input("Enter second y value:  "))
 x_3= int(input("Enter third x value:  "))
 y_3= int(input("Enter third y value:  "))
-
+#Asking the values of the points.
 
 points = [(x_1,y_1),(x_2,y_2),(x_3,y_3)]
 distances= []  
@@ -14,11 +14,15 @@ distances= []
 def euclideanDistance(point1,point2):
     distance= math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
     return distance
+#Calculating the distance.
+
 
 for a in range (len(points)):
     for b in range(a + 1, len(points)):
         x = euclideanDistance(points[a], points[b])
         distances.append(x)
+#Adding distances to the list.
+
         
 print("Calculated distances are: ", distances)        
 print("Minimum distance is: " ,min(distances))
